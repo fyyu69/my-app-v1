4faff71abb7bcf788c36f4e62347ad1f6737fe03
Patrick Yu <fyyu69@outlook.com>
2020-10-04 20:04:46 -0700

    Add react router to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,83 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { 
+  AppBar, 
+  Grid, 
+  Toolbar, 
+  Typography
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-
-  },
   title: {
-
+    fontSize: '28px',
+    fontWeight: 700
   },
-  button: {
-
+  navlist: {
+    listStyle: 'none'
+  },
+  navlistItem: {
+    marginRight: theme.spacing(4)
+  },
+  navlink: {
+    color: theme.palette.common.white,
+    fontSize: '18px',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline'
+    },
+    '&.active': {
+      color: '#ffb74d',
+      fontWeight: 700
+    }
   }
 }));
+
+function NavLink({ label, to, activeOnlyWhenExact, classNames }) {
+  let match = useRouteMatch({
+    path: to,
+    exact: activeOnlyWhenExact
+  });
+
+  return (
+    <div>
+      <Link to={to} className={`${classNames} ${match ? 'active' : ''}`}>{label}</Link>
+    </div>
+  )
+}
 
 export default function Header() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography className={classes.title}>
-            Patrick Yu
-          </Typography>
-          <Link to='/projects' color='secondary'>Projects</Link>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position='static'>
+      <Toolbar>
+        <Grid
+          container
+          justify="flex-start"
+          alignItems="center">
+          <Grid item xs={4}>
+            <Typography variant='h1' className={classes.title}>
+              Patrick Yu
+            </Typography>
+          </Grid>
+          <Grid item xs={8}>
+            <Grid 
+              container
+              justify="flex-end"
+              alignItems="center"
+              component='ul'
+              className={classes.navlist}>
+              <Grid item component='li' className={classes.navlistItem}>
+                <NavLink label='Home' to='/' activeOnlyWhenExact={true} classNames={classes.navlink}/>
+              </Grid>
+              <Grid item component='li' className={classes.navlistItem}>
+                <NavLink label='Projects' to='/projects' activeOnlyWhenExact={false} classNames={classes.navlink}/>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div>
+    <main>
       
-    </div>
+    </main>
   )
 }
